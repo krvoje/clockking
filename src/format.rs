b2b219027d53c05a04ca7b02ebb6367b43f61186
Krvoje <hperadin@gmail.com>
2022-05-08 01:41:09 +0200

    Add tests for time formatting and date entries

--- src/format.rs
+++ src/format.rs
@@ -2,15 +2,15 @@ use chrono::{NaiveTime, Timelike};
 
 use crate::granularity::Granularity;
 
-pub fn format_hms_with_prompt(granularity: Granularity, prompt: &str, total_minutes: i64) -> String {
-    format!("{}:\t{}", prompt, format_hms(granularity, total_minutes))
+pub fn format_hms_with_prompt(granularity: Granularity, prompt: &str, total_seconds: i64) -> String {
+    format!("{}:\t{}", prompt, format_hms(granularity, total_seconds))
 }
 
 pub fn format_hms(granularity: Granularity, total_seconds: i64) -> String {
     let hours = total_seconds / 3600;
-    let minutes = total_seconds / 60 % 60;
+    let minutes = (total_seconds / 60) % 60;
     let seconds = total_seconds % 60;
-    
+
     match granularity {
         Granularity::Relaxed => format!("{}h", hours),
         Granularity::Reasonable => format!("{}h {:02$}m", hours, minutes / 30 * 30, 2),
@@ -21,9 +21,13 @@ pub fn format_hms(granularity: Granularity, total_seconds: i64) -> String {
     }
 }
 
+pub fn format_naive_time(granularity: Granularity, it: NaiveTime) -> String {
+    format_clock(granularity, it.hour(), it.minute(), it.second())
+}
+
 pub fn format_clock(granularity: Granularity, hours: u32, minutes: u32, seconds: u32) -> String {
     match granularity {
-        Granularity::Relaxed => format!("{}:00", hours),
+        Granularity::Relaxed => format!("{:01$}:00", hours, 2),
         Granularity::Reasonable => format!("{:02$}:{:02$}", hours, (minutes / 30) * 30, 2),
         Granularity::Detailed => format!("{:02$}:{:02$}", hours, (minutes / 15) * 15, 2),
         Granularity::Paranoid => format!("{:02$}:{:02$}", hours, (minutes / 5) * 5, 2),
@@ -32,6 +36,184 @@ pub fn format_clock(granularity: Granularity, hours: u32, minutes: u32, seconds:
     }
 }
 
-pub fn format_naive_time(granularity: Granularity, it: NaiveTime) -> String {
-    format_clock(granularity, it.hour(), it.minute(), it.second())
+#[cfg(test)]
+mod format_hms_test {
+    use crate::format::format_hms;
+    use crate::Granularity;
+
+    #[test]
+    fn format_hms_relaxed() {
+        (0..24).for_each(|hour| {
+            (0..60).for_each(|minute| {
+                (0..60).for_each(|second| {
+                    assert_eq!(
+                        format_hms(Granularity::Relaxed, hour * 3600 + minute * 60 + second),
+                        format!("{}h", hour)
+                    )
+                })
+            })
+        });
+    }
+
+    #[test]
+    fn format_hms_reasonable() {
+        (0..24).for_each(|hour| {
+            (0..60).for_each(|minute| {
+                (0..60).for_each(|second| {
+                    assert_eq!(
+                        format_hms(Granularity::Reasonable, hour * 3600 + minute * 60 + second),
+                        format!("{}h {:02$}m", hour, minute / 30 * 30, 2)
+                    )
+                })
+            })
+        });
+    }
+
+    #[test]
+    fn format_hms_detailed() {
+        (0..24).for_each(|hour| {
+            (0..60).for_each(|minute| {
+                (0..60).for_each(|second| {
+                    assert_eq!(
+                        format_hms(Granularity::Detailed, hour * 3600 + minute * 60 + second),
+                        format!("{}h {:02$}m", hour, minute / 15 * 15, 2)
+                    )
+                })
+            })
+        });
+    }
+
+    #[test]
+    fn format_hms_paranoid() {
+        (0..24).for_each(|hour| {
+            (0..60).for_each(|minute| {
+                (0..60).for_each(|second| {
+                    assert_eq!(
+                        format_hms(Granularity::Paranoid, hour * 3600 + minute * 60 + second),
+                        format!("{}h {:02$}m", hour, minute / 5 * 5, 2)
+                    )
+                })
+            })
+        });
+    }
+
+    #[test]
+    fn format_hms_ocd() {
+        (0..24).for_each(|hour| {
+            (0..60).for_each(|minute| {
+                (0..60).for_each(|second| {
+                    assert_eq!(
+                        format_hms(Granularity::OCD, hour * 3600 + minute * 60 + second),
+                        format!("{}h {:02$}m", hour, minute, 2)
+                    )
+                })
+            })
+        });
+    }
+
+    #[test]
+    fn format_hms_scientific() {
+        (0..24).for_each(|hour| {
+            (0..60).for_each(|minute| {
+                (0..60).for_each(|second| {
+                    assert_eq!(
+                        format_hms(Granularity::Scientific, hour * 3600 + minute * 60 + second),
+                        format!("{}h {:03$}m {:03$}s", hour, minute, second, 2)
+                    )
+                })
+            })
+        });
+    }
+
+}
+
+#[cfg(test)]
+mod format_clock_test {
+    use crate::format::format_clock;
+    use crate::Granularity;
+
+    #[test]
+    fn format_clock_relaxed() {
+        (0..24).for_each(|hour| {
+            (0..60).for_each(|minute| {
+                (0..60).for_each(|second| {
+                    assert_eq!(
+                        format_clock(Granularity::Relaxed, hour, minute, second),
+                        format!("{:01$}:00", hour, 2)
+                    )
+                })
+            })
+        });
+    }
+
+    #[test]
+    fn format_clock_reasonable() {
+        (0..24).for_each(|hour| {
+            (0..60).for_each(|minute| {
+                (0..60).for_each(|second| {
+                    assert_eq!(
+                        format_clock(Granularity::Reasonable, hour, minute, second),
+                        format!("{:02$}:{:02$}", hour, minute / 30 * 30, 2)
+                    )
+                })
+            })
+        });
+    }
+
+    #[test]
+    fn format_clock_detailed() {
+        (0..24).for_each(|hour| {
+            (0..60).for_each(|minute| {
+                (0..60).for_each(|second| {
+                    assert_eq!(
+                        format_clock(Granularity::Detailed, hour, minute, second),
+                        format!("{:02$}:{:02$}", hour, minute / 15 * 15, 2)
+                    )
+                })
+            })
+        });
+    }
+
+    #[test]
+    fn format_clock_paranoid() {
+        (0..24).for_each(|hour| {
+            (0..60).for_each(|minute| {
+                (0..60).for_each(|second| {
+                    assert_eq!(
+                        format_clock(Granularity::Paranoid, hour, minute, second),
+                        format!("{:02$}:{:02$}", hour, minute / 5 * 5, 2)
+                    )
+                })
+            })
+        });
+    }
+
+    #[test]
+    fn format_clock_ocd() {
+        (0..24).for_each(|hour| {
+            (0..60).for_each(|minute| {
+                (0..60).for_each(|second| {
+                    assert_eq!(
+                        format_clock(Granularity::OCD, hour, minute, second),
+                        format!("{:02$}:{:02$}", hour, minute, 2)
+                    )
+                })
+            })
+        });
+    }
+
+    #[test]
+    fn format_clock_scientific() {
+        (0..24).for_each(|hour| {
+            (0..60).for_each(|minute| {
+                (0..60).for_each(|second| {
+                    assert_eq!(
+                        format_clock(Granularity::Scientific, hour, minute, second),
+                        format!("{:03$}:{:03$}:{:03$}", hour, minute, second, 2)
+                    )
+                })
+            })
+        });
+    }
+
 }
--- src/time_picker.rs
+++ src/time_picker.rs
@@ -36,11 +36,20 @@ pub fn get_time(s: &mut Cursive, col: ClockEntryColumn) -> NaiveTime {
     }).expect(&format!("{} should be defined", col.as_str()))
 }
 
-pub fn parse_time(granularity: Granularity, value: &str) -> NaiveTime {
-    if granularity == Granularity::Scientific {
+fn parse_time(granularity: Granularity, value: &str) -> NaiveTime {
+    let time = if granularity == Granularity::Scientific {
         NaiveTime::parse_from_str(value, "%H:%M:%S").expect("Unable to parse time from selection")
     } else {
         NaiveTime::parse_from_str(value, "%H:%M").expect("Unable to parse time from selection")
+    }.with_nanosecond(0).unwrap();
+
+    match granularity {
+        Granularity::Relaxed => {time.with_minute(0).unwrap().with_second(0).unwrap()}
+        Granularity::Reasonable => {time.with_minute(time.minute() / 30 * 30).unwrap().with_second(0).unwrap()}
+        Granularity::Detailed => {time.with_minute(time.minute() / 15 * 15).unwrap().with_second(0).unwrap()}
+        Granularity::Paranoid => {time.with_minute(time.minute() / 5 * 5).unwrap().with_second(0).unwrap()}
+        Granularity::OCD => {time.with_second(0).unwrap()}
+        Granularity::Scientific => time,
     }
 }
 
@@ -69,4 +78,148 @@ fn daily_clock_entries(granularity: Granularity) -> Vec<String> {
             }).collect::<Vec<String>>()
         }).collect::<Vec<String>>()
     }).collect()
+}
+
+#[cfg(test)]
+mod parse_time_test {
+    use chrono::NaiveTime;
+
+    use crate::Granularity;
+    use crate::time_picker::parse_time;
+
+    #[test]
+    fn parse_time_relaxed() {
+        (0..24).for_each(|hour| {
+            (0..60).for_each(move |minute| {
+                assert_eq!(
+                    parse_time(Granularity::Relaxed, format!("{:02$}:{:02$}", hour, minute, 2).as_str()),
+                    NaiveTime::from_hms(hour, 0, 0)
+                )
+            })
+        });
+    }
+
+    #[test]
+    fn parse_time_reasonable() {
+        (0..24).for_each(|hour| {
+            (0..60).for_each(move |minute| {
+                assert_eq!(
+                    parse_time(Granularity::Reasonable, format!("{:02$}:{:02$}", hour, minute, 2).as_str()),
+                    NaiveTime::from_hms(hour, minute / 30 * 30, 0)
+                )
+            })
+        });
+    }
+
+    #[test]
+    fn parse_time_detailed() {
+        (0..24).for_each(|hour| {
+            (0..60).for_each(move |minute| {
+                assert_eq!(
+                    parse_time(Granularity::Detailed, format!("{:02$}:{:02$}", hour, minute, 2).as_str()),
+                    NaiveTime::from_hms(hour, minute / 15 * 15, 0)
+                )
+            })
+        });
+    }
+
+    #[test]
+    fn parse_time_paranoid() {
+        (0..24).for_each(|hour| {
+            (0..60).for_each(move |minute| {
+                assert_eq!(
+                    parse_time(Granularity::Paranoid, format!("{:02$}:{:02$}", hour, minute, 2).as_str()),
+                    NaiveTime::from_hms(hour, minute / 5 * 5, 0)
+                )
+            })
+        });
+    }
+
+    #[test]
+    fn parse_time_ocd() {
+        (0..24).for_each(|hour| {
+            (0..60).for_each(move |minute| {
+                assert_eq!(
+                    parse_time(Granularity::OCD, format!("{:02$}:{:02$}", hour, minute, 2).as_str()),
+                    NaiveTime::from_hms(hour, minute, 0)
+                )
+            })
+        });
+    }
+}
+
+#[cfg(test)]
+mod daily_clock_entries_test {
+    use crate::Granularity;
+    use crate::time_picker::daily_clock_entries;
+
+    #[test]
+    fn daily_clock_entries_relaxed() {
+        assert_eq!(
+            daily_clock_entries(Granularity::Relaxed),
+            (0..24).map(|hour|{ format!("{:01$}:00", hour, 2) }).collect::<Vec<String>>()
+        );
+    }
+
+    #[test]
+    fn daily_clock_entries_reasonable() {
+        assert_eq!(
+            daily_clock_entries(Granularity::Reasonable),
+            (0..24).flat_map(|hour|{
+                [0, 30].iter().map(|minute|{
+                    format!("{:02$}:{:02$}", hour, minute, 2)
+                }).collect::<Vec<String>>()
+            }).collect::<Vec<String>>()
+        );
+    }
+
+    #[test]
+    fn daily_clock_entries_detailed() {
+        assert_eq!(
+            daily_clock_entries(Granularity::Detailed),
+            (0..24).flat_map(|hour|{
+                [0, 15, 30, 45].iter().map(|minute|{
+                    format!("{:02$}:{:02$}", hour, minute, 2)
+                }).collect::<Vec<String>>()
+            }).collect::<Vec<String>>()
+        );
+    }
+
+    #[test]
+    fn daily_clock_entries_paranoid() {
+        assert_eq!(
+            daily_clock_entries(Granularity::Paranoid),
+            (0..24).flat_map(|hour|{
+                [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55].iter().map(|minute|{
+                    format!("{:02$}:{:02$}", hour, minute, 2)
+                }).collect::<Vec<String>>()
+            }).collect::<Vec<String>>()
+        );
+    }
+
+    #[test]
+    fn daily_clock_entries_ocd() {
+        assert_eq!(
+            daily_clock_entries(Granularity::OCD),
+            (0..24).flat_map(|hour|{
+                (0..60).map(|minute|{
+                    format!("{:02$}:{:02$}", hour, minute, 2)
+                }).collect::<Vec<String>>()
+            }).collect::<Vec<String>>()
+        );
+    }
+
+    #[test]
+    fn daily_clock_entries_scientific() {
+        assert_eq!(
+            daily_clock_entries(Granularity::Scientific),
+            (0..24).flat_map(|hour| {
+                (0..60).flat_map(|minute| {
+                    (0..60).map(|second| {
+                        format!("{:03$}:{:03$}:{:03$}", hour, minute, second, 2)
+                    }).collect::<Vec<String>>()
+                }).collect::<Vec<String>>()
+            }).collect::<Vec<String>>()
+        );
+    }
 }
